c18ebfc1c990cc6f621de75c6819f3eaf40868c5
abaz07 <abazov.islam2017@yandex.ru>
2024-05-12 16:15:59 +0300

    collections2

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
+//private final String temp = "с://temp";
+//private static final List<Integer> Nums = List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7);
+//public static void main(String[] args) {
+//    List<Integer> nums = List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7);
+//    for (Integer num : nums){
+//        if(num % 2 != 0){
+//            System.out.println(num);
+//        }
+//    }
+//}
